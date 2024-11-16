119e90b9c7547b53c43fa10badd51272c19860e4
Dominic <siewdominic1@gmail.com>
2024-11-17 00:01:03 +0800

    Implement connection to Kafka

--- spark_stream.py
+++ spark_stream.py
@@ -5,6 +5,14 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType
 
+def create_keyspace(session):
+    session.execute("""
+        CREATE KEYSPACE IF NOT EXISTS spark_streams
+        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
+    """)
+
+    print("Keyspace created successfully!")
+
 def create_spark_connection():
     s_conn = None
 
@@ -43,6 +51,22 @@ def create_cassandra_connection():
         logging.error(f"Could not create cassandra connection due to {e}")
         return None
     
+def connect_to_kafka(spark_conn):
+    spark_df = None
+    try:
+        spark_df = spark_conn.readStream \
+            .format('kafka') \
+            .option('kafka.bootstrap.servers', 'localhost:9092') \
+            .option('subscribe', 'users_created') \
+            .option('startingOffsets', 'earliest') \
+            .load()
+        logging.info("kafka dataframe created successfully")
+    except Exception as e:
+        logging.warning(f"kafka dataframe could not be created because: {e}")
+
+    return spark_df
+
+
 def create_table(session):
     session.execute("""
     CREATE TABLE IF NOT EXISTS spark_streams.created_users (
@@ -95,9 +119,13 @@ if __name__ == "__main__":
     spark_conn = create_spark_connection()
 
     if spark_conn:
+        # connect to kafka with spark connection 
+        df = connect_to_kafka(spark_conn)
         session = create_cassandra_connection()
 
         if session: 
+            create_keyspace(session)
             create_table(session)
+            insert_data(session)
 
 
